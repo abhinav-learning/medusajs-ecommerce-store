d147b831bfbdbd48f5add73984022bb94a62ed6c
Abhinav <tech01@publiqverse.io>
2024-11-21 21:31:49 +0530

    Fixed the Jobs to retrieve the product count

--- my-medusa-store/src/jobs/hello-world.ts
+++ my-medusa-store/src/jobs/hello-world.ts
@@ -1,9 +1,22 @@
-// the scheduled-job function
-export default function () {
-    console.log("Time to say hello world!")
+import {
+    IProductModuleService,
+    MedusaContainer,
+} from "@medusajs/framework/types"
+import { Modules } from "@medusajs/framework/utils"
+
+export default async function myCustomJob(
+    container: MedusaContainer
+) {
+    const productModuleService: IProductModuleService =
+        container.resolve(Modules.PRODUCT)
+
+    const [, count] = await productModuleService.listAndCountProducts()
+
+    console.log(
+        `Time to check products! You have ${count} product(s)`
+    )
 }
 
-// the job's configurations
 export const config = {
     name: "every-minute-message",
     // execute every minute
